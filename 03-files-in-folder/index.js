9841c2cc293ea7b089c5e0e0916a8fc57734ad7d
olegov-lab <olego8760@gmail.com>
2021-10-30 13:33:20 +0300

    feat:03

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const { stat } = fs;
+const path = require("path");
+
+fs.readdir(
+  path.join(__dirname, "secret-folder"),
+  { withFileTypes: true },
+  (err, files) => {
+    console.log("\nCurrent directory files:");
+    if (err) console.log(err);
+    else {
+      files.forEach((file) => {
+        if (file.isFile()) {
+          fs.stat(
+            path.join(__dirname, "secret-folder", file.name),
+            (err, stats) => {
+              const nameFile = path.parse(file.name).name;
+              const extFile = path.extname(file.name).slice(1);
+              const sizeFile = stats.size * 0.001;
+
+              console.log(`${nameFile} - ${extFile} - ${sizeFile}kb`);
+            }
+          );
+        }
+      });
+    }
+  }
+);
