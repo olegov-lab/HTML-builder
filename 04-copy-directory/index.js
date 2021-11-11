6d12d75e08462b2a807f21218f8ea3d07acdf8bc
olegov-lab <olego8760@gmail.com>
2021-11-11 21:00:33 +0300

    feat:fix

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,23 +5,42 @@ const path = require("path");
 const filePlace = path.join(__dirname, "files");
 const fileCopy = path.join(__dirname, "files-copy");
 
-fs.mkdir(fileCopy, { recursive: true }, (err) => {
-  if (err) throw err;
-});
+fs.promises
+  .rm(fileCopy, { recursive: true, force: true })
+  .then(
+    () => {
+      return fs.promises.mkdir(fileCopy, { recursive: true });
+    },
 
-fs.readdir(filePlace, { withFileTypes: true }, (err, files) => {
-  if (err) console.log(err);
-  else {
-    files.forEach((file) => {
-      fs.copyFile(
-        path.join(filePlace, file.name),
-        path.join(fileCopy, file.name),
-        // COPYFILE_EXCL,
-        (err) => {
-          if (err) throw err; // не удалось скопировать файл. Он уже существует?
-          console.log("Файл успешно скопирован");
+    (error) => {
+      if (error) console.error(error.message);
+    }
+  )
+  .then(
+    () => {
+      return fs.readdir(filePlace, { withFileTypes: true }, (err, files) => {
+        if (err) console.log(err);
+        else {
+          files.forEach((file) => {
+            fs.copyFile(
+              path.join(filePlace, file.name),
+              path.join(fileCopy, file.name),
+              // COPYFILE_EXCL,
+              (err) => {
+                if (err) throw err; // не удалось скопировать файл. Он уже существует?
+                console.log("Файл успешно скопирован");
+              }
+            );
+          });
         }
-      );
-    });
-  }
-});
+      });
+    },
+
+    (error) => {
+      if (error) console.error(error.message);
+    }
+  );
+
+// fs.mkdir(fileCopy, { recursive: true }, (err) => {
+//   if (err) throw err;
+// });
